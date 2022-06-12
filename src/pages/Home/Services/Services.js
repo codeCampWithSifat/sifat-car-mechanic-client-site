c64254e682d2c552e77c33300f71b06443f32bd5
unknown <sayedhossasishifat706@gmail.com>
2022-06-13 01:24:26 +0600

    motamuti

---
+++ src/pages/Home/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const services = [
+    {name: "Replace Tyre", price : 2000 , time : 2, img : "https://i.ibb.co/1L3FMVy/3.jpg"}
+]
+
+const Services = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Services;
